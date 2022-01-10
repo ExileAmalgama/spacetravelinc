695d68807cacbfb598cd3d790c11d0ff39f40057
ExileAmalgama <91181399+ExileAmalgama@users.noreply.github.com>
2022-01-10 21:02:19 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+// Map module
+function initMap() {
+  // Your location
+  const loc = { lat: 50.01483252651805, lng: 36.22826614198718, };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 15,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
